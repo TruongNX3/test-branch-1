325d328d4d705e0fdf89c7ef771809804f89f30a
SON NGUYEN <myantj3m@gmail.com>
2018-12-08 13:05:51 +0700

    nhu cax 1

--- src/com/company/Tuananh.java
+++ src/com/company/Tuananh.java
@@ -2,6 +2,7 @@ package com.company;
 
 public class Tuananh {
     public static void main(String[] args) {
+        System.out.println("cuoc song ma");
         System.out.println("nhu cax");
     }
 }
