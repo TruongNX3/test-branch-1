a731d9de2a5aceecd595e9038569a014c55b34c2
sonnh <hoangson.101197@gmail.com>
2018-12-08 12:50:21 +0700

    sửa demo

--- src/com/company/Demo.java
+++ src/com/company/Demo.java
@@ -2,6 +2,6 @@ package com.company;
 
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("mãi yêu");
+        System.out.println("mãi yêu chos");
     }
 }
